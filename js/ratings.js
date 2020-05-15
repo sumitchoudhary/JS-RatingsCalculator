dad136cc213162ba457e351833b7ae889f38bbd8
sumit <sumit.choudhary@eclinicalworks.com>
2020-05-15 18:19:37 -0400

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,17 +1,20 @@
-function collect_ratings()
-{
+function collect_ratings() {
     const ratings = {count: 0, sum: 0, average: 0};
     let rating = 0;
-    const elements = document.querySelectorAll('.rating')
+    const elements = document.querySelectorAll('.rating');
 
 
     elements.forEach(element => {
-   rating = parseInt(element.id.replace('star',''));
-            ratings.count += parseInt(element.value);
-            ratings.sum += parseInt(element.value)*rating;
+        rating = parseInt(element.id.replace('star', ''));
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
 
-            console.log(ratings.count );                                    }
+    });
 
-                               );
+    if (ratings.count !== 0) {
 
+        ratings.average = ratings.sum / ratings.count
+
+
+    }
 }
